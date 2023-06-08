fa4834039e9ff4db1286bcf65f1a48e0b0fd2f70
Domysaty <satyrkoo@gmail.com>
2023-06-08 23:35:55 +0200

    ff

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const currentTimeKey = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime({ seconds }) {
+  localStorage.setItem(currentTimeKey, seconds);
+}
+
+function restoreCurrentTime() {
+  const currentTime = localStorage.getItem(currentTimeKey);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
+
+restoreCurrentTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const localStorageKey = 'feedback-form-state';
+
+// Відстежуємо подію input і зберігаємо значення полів у локальне сховище
+form.addEventListener('input', throttle(saveFormState, 500));
+
+// Під час завантаження сторінки заповнюємо поля форми значеннями з локального сховища
+window.addEventListener('DOMContentLoaded', populateFormFields);
+
+// Очищаємо сховище та поля форми під час сабміту форми
+form.addEventListener('submit', handleSubmit);
+
+function saveFormState() {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formState));
+}
+
+function populateFormFields() {
+  const savedFormState = localStorage.getItem(localStorageKey);
+  if (savedFormState) {
+    const formState = JSON.parse(savedFormState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log(formState);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+}
